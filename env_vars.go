1a9226af2293859c7c0366168b38b8c4b9b4cb6b
Adrian Duke <adrian.duke@gmail.com>
2016-06-23 17:15:44 +0100

    Added environmental variables source, can work with or without a prefix

---
+++ env_vars.go
@@ -0,0 +1,56 @@
+package configr
+
+import (
+	"os"
+	"strings"
+)
+
+const (
+	EnvVarSeparator = "_"
+)
+
+type EnvVars struct {
+	prefix             string
+	envVarsToUnmarshal []string
+	keysToUnmarshal    []string
+}
+
+func NewEnvVars(prefix string) *EnvVars {
+	return &EnvVars{
+		prefix:             prefix,
+		envVarsToUnmarshal: []string{},
+	}
+}
+
+func (e *EnvVars) Unmarshal() (map[string]interface{}, error) {
+	returnMap := map[string]interface{}{}
+
+	for i, envVarKey := range e.envVarsToUnmarshal {
+		if envVarValue, exists := os.LookupEnv(envVarKey); exists {
+			returnMap[e.keysToUnmarshal[i]] = envVarValue
+		}
+	}
+
+	return returnMap, nil
+}
+
+func (e *EnvVars) KeysToUnmarshal(keys []string, keySplitter KeySplitter) {
+	e.keysToUnmarshal = keys
+
+	for _, key := range keys {
+		e.envVarsToUnmarshal = append(
+			e.envVarsToUnmarshal,
+			toEnvVarKey(e.prefix, key, keySplitter),
+		)
+	}
+}
+
+func toEnvVarKey(prefix, key string, keySplitter KeySplitter) string {
+	keyParts := keySplitter(strings.ToUpper(key))
+
+	if prefix != "" {
+		keyParts = append([]string{strings.ToUpper(prefix)}, keyParts...)
+	}
+
+	return strings.Join(keyParts, EnvVarSeparator)
+}
